07ece03a1cb5abf345dd9ecb94944427b01298ea
hyeonggeun2 <dhgudrms12@gmail.com>
2019-12-17 16:23:18 +0900

    create question, choice's model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    # ForeignKey = 다수 : 1의 관계 설정 <choice 하나에 여러개의 question 이 붙을 수 있게 됨>
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
